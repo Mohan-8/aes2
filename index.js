362e2d5a7d5689bd879c1dc43efc44c82d0a906e
Mohan-8 <smohanakrishnan82@gmail.com>
2024-11-13 17:36:51 +0530

    updated

--- index.js
+++ index.js
@@ -52,11 +52,7 @@ console.log("Telegram Bot is running");
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 10000, // Increase to 10 seconds
-  })
+  .connect(process.env.MONGO_URI, {})
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
